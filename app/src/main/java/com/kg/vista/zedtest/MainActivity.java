3d28a6925c98906560bb2d59fa6fb6191ca3a9ad
Elbar <Elbar_israilov@mail.ru>
2018-01-01 01:34:28 +0600

    +Added api service

--- app/src/main/java/com/kg/vista/zedtest/MainActivity.java
+++ app/src/main/java/com/kg/vista/zedtest/MainActivity.java
@@ -2,12 +2,61 @@ package com.kg.vista.zedtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.kg.vista.zedtest.api.ApiService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String ROOT_URL =  "http://www.mocky.io";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadJSON();
     }
+
+
+    private void loadJSON() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(ROOT_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ApiService request = retrofit.create(ApiService.class);
+        Call<ResponseBody> call = request.getMockyData();
+        call.enqueue(new Callback<ResponseBody>() {
+
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                try {
+                    Toast.makeText(MainActivity.this, response.body().string(), Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.d("Error", t.getMessage());
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/kg/vista/zedtest/api/ApiService.java
+++ app/src/main/java/com/kg/vista/zedtest/api/ApiService.java
@@ -1,5 +1,13 @@
 package com.kg.vista.zedtest.api;
 
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface ApiService {
+
+    @GET("/v2/5a488f243000004c15c3c57e")
+    Call<ResponseBody> getMockyData();
+
 }
